b76e134afb532e07a1a7b0ddb555d7f03d64aa3f
AgentFabulous <kshitijgm@gmail.com>
2020-09-22 10:04:05 +0200

    envsetup: Correct assignment of a few variables
    - Not sure how they got here, but it causes issues when roomservice finishes syncing deps
    - Correct syntax to have the variables get set correctly
    
    Change-Id: Ib358756c9f6b445628b818e37d8e90617ceab925

--- envsetup.sh
+++ envsetup.sh
@@ -662,10 +662,11 @@ function lunch()
         return 1
     fi
 
-    TARGET_PRODUCT=$product \
-    TARGET_BUILD_VARIANT=$variant \
-    TARGET_PLATFORM_VERSION=$version \
+    TARGET_PRODUCT=$product
+    TARGET_BUILD_VARIANT=$variant
+    TARGET_PLATFORM_VERSION=$version
     build_build_var_cache
+
     if [ $? -ne 0 ]
     then
         # if we can't find the product, try to grab it from our github

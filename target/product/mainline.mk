2584ffc4da4075b89476b08bf1696ec2bac5b00e
Ritzz <riteshm321@gmail.com>
2020-09-24 11:47:35 +0200

    Revert "product: Exclude AOSP apn list on custom build"
    This reverts commit c85dd5c06caeae669f44d45c4ca56c9b2c46a60e.

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -38,6 +38,4 @@ PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
 
-ifeq ($(CUSTOM_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
-endif
